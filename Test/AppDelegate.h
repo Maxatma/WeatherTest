aa72ac12f250b367a1c4bccea412c9b392bb867e
Alexander <maxatma@CPE1470102.tvcom.net.ua>
2015-01-15 05:19:53 +0200

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Alexander on 1/15/15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Alexander on 1/15/15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
